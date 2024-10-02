21034cb81cd57115da461616fd0cb4e244a2a9c6
Zer0Tora <101511043+Zer0Tora@users.noreply.github.com>
2024-10-02 12:07:40 +0300

    Покрытие тестами методов summ и diff

---
+++ src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @ParameterizedTest (name = "#{index} Сумма {0} и {1} равна {2}")
+    @CsvSource({"2, 3, 5", "5, -10, -5", "0, 0, 0"})
+    @DisplayName("Тест суммы двух чисел")
+    @Tags({})
+    void summ(int a, int b, int resultExp) {
+        Calculator calc = new Calculator();
+        int resultSum = calc.summ(a, b);
+        Assertions.assertEquals(resultExp, resultSum, "Ошибка сложения");
+    }
+
+    @ParameterizedTest (name = "#{index} Разность {0} и {1} равна {2}")
+    @CsvSource({"7, 3, 4", "5, -10, 15", "0, 0, 0"})
+    @DisplayName("Тест разности двух чисел")
+    @Tags({})
+    void diff(int a, int b, int resultExp) {
+        Calculator calc = new Calculator();
+        int resultSum = calc.diff(a, b);
+        Assertions.assertEquals(resultExp, resultSum, "Ошибка вычитания");
+    }
+}
