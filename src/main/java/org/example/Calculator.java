5ff3fc9411f4760d1dd224d18114b6aaa5c101c6
Zer0Tora <101511043+Zer0Tora@users.noreply.github.com>
2024-10-02 11:30:42 +0300

    Добавлен diff в калькулятор

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -3,7 +3,13 @@ package org.example;
 public class Calculator {
     public int summ(int a, int b) {;
         int sumResult = a + b;
-        System.out.printf("Сумма %d и %d равна %d", a, b, sumResult);
+        System.out.printf("Сумма %d и %d равна %d\n", a, b, sumResult);
         return sumResult;
+    }
+
+    public int diff(int a, int b) {;
+        int diffResult = a - b;
+        System.out.printf("Разность %d и %d равна %d\n", a, b, diffResult);
+        return diffResult;
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator sum = new Calculator();
-        sum.summ(1, 2);
+        Calculator calc = new Calculator();
+        calc.summ(1, 2);
+        calc.diff(5, 6);
     }
 }
