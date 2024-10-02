47b6cca880e0c50ee681618423e03ad0e719ac3d
Zer0Tora <101511043+Zer0Tora@users.noreply.github.com>
2024-10-02 11:18:24 +0300

    Добавлен summ в калькулятор

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Calculator {
+    public int summ(int a, int b) {;
+        int sumResult = a + b;
+        System.out.printf("Сумма %d и %d равна %d", a, b, sumResult);
+        return sumResult;
+    }
+}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator sum = new Calculator();
+        sum.summ(1, 2);
     }
 }
